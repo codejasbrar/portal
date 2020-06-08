ab1a6a62f3870210f70588c63817f3b7f893b6e5
gauzeee <gauzeee@gmail.com>
2020-06-08 16:09:40 +0300

    fix env var api host

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,6 @@ interface Config {
 
 const config: Config = {
   hostName: process.env.REACT_APP_HOST,
-  apiHostName: 'https://test-api-physician.insidetracker.com',
+  apiHostName: process.env.REACT_APP_API_HOST,
 } as Config;
 export default config
-
-//process.env.REACT_APP_API_HOST
