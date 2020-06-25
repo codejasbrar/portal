1feac84b3d930fde812af85242162f2e597659b8
Kate <kaliago@yandex.ru>
2020-06-25 13:58:43 +0300

    ITRACKER-11188 Orders Pending Approval

--- src/components/Table/SearchMobile/SearchBarMobile.tsx
+++ src/components/Table/SearchMobile/SearchBarMobile.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import styles from "./SearchBarMobile.module.scss";
 import {ReactComponent as SearchIcon} from "../../../icons/search.svg";
 
-export default () => <>
-  <div className={styles.search}>
-    <input className={styles.searchInput} type={'text'} placeholder="Search" />
-    <span className={styles.searchIcon}><SearchIcon /></span>
-  </div>
-</>
+const SearchBarMobile = () =>
+  <>
+    <div className={styles.search}>
+      <input className={styles.searchInput} type={'text'} placeholder="Search" />
+      <span className={styles.searchIcon}><SearchIcon /></span>
+    </div>
+  </>
+
+export default SearchBarMobile;
--- src/pages/OrdersPages/PendingOrdersPage/PendingOrdersPage.tsx
+++ src/pages/OrdersPages/PendingOrdersPage/PendingOrdersPage.tsx
@@ -31,7 +31,7 @@ function useCurrentWitdh() {
 
 const columns = [
   {
-    name: "orderID",
+    name: "id",
     label: "Order ID",
     options: {
       filter: true,
@@ -52,7 +52,7 @@ const columns = [
     }
   },
   {
-    name: "customerID",
+    name: "customerId",
     label: "Customer ID",
     options: {
       filter: true,
@@ -70,24 +70,24 @@ const columns = [
 ];
 
 const data = [
-  {orderID: "23244", received: "4/23/2020", customerID: "33267", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2019", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2018", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "4/23/2020", customerID: "33267", criteriaMet: false},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "7777", received: "7/23/2020", customerID: "7778", criteriaMet: true},
-  {orderID: "9830", received: "9/23/2020", customerID: "33267", criteriaMet: false},
+  {id: "23244", received: "4/23/2020", customerId: "33267", criteriaMet: true},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "4/23/2019", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2018", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "4/23/2020", customerId: "33267", criteriaMet: false},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "7777", received: "7/23/2020", customerId: "7778", criteriaMet: true},
+  {id: "9830", received: "9/23/2020", customerId: "33267", criteriaMet: false},
 ];
 
 const options: MUIDataTableOptions = {
@@ -129,6 +129,14 @@ const options: MUIDataTableOptions = {
   </div>
 } as MUIDataTableOptions;
 
+const NoMatches = () => (
+  <div className={styles.sorry}>
+    <p className={styles.sorryText}>
+      No results found
+    </p>
+  </div>
+);
+
 const PendingOrdersPage = () =>
   <section className={styles.orders}>
     <Link to={'/orders/navigation'} className={`${styles.menuLink} ${styles.showTabletHorizontal}`}>
@@ -152,16 +160,17 @@ const PendingOrdersPage = () =>
         {data.map((item: any, i) => (
           <div key={i} className={styles.mobileOrdersItem}>
             <p className={styles.mobileOrdersTitle}>Order
-              ID: <span className={styles.mobileOrdersText}>{item.orderID}</span></p>
+              ID: <span className={styles.mobileOrdersText}>{item.id}</span></p>
             <p className={styles.mobileOrdersTitle}>Received: <span className={styles.mobileOrdersText}>{item.received}</span>
             </p>
             <p className={styles.mobileOrdersTitle}>Customer
-              ID: <span className={styles.mobileOrdersText}>{item.customerID}</span></p>
+              ID: <span className={styles.mobileOrdersText}>{item.customerId}</span></p>
             <p className={styles.mobileOrdersTitle}>Criteria
               met: <span className={styles.mobileOrdersText}>{item.criteriaMet ? "Yes" : "No"}</span></p>
             <button className={styles.btnPrimary}>Approve</button>
           </div>
         ))}
+        {/*<NoMatches/>*/}
       </div>
     }
   </section>
