cd5c5b8b87c773c2a4e9c1f91e4d908cae319eed
Kate <kaliago@yandex.ru>
2020-06-29 17:00:11 +0300

    ITRACKER-11201 fixes

--- src/pages/OrdersPages/TestPendingOrdersPage/TestPendingOrdersPage.tsx
+++ src/pages/OrdersPages/TestPendingOrdersPage/TestPendingOrdersPage.tsx
@@ -166,12 +166,12 @@ const TestsPage = () => {
   const testsToView = data
     .map(reformatDate)
     .filter(searchFilter)
-    .sort(((a: any, b: any) => {
-      const aDate = new Date(a.received);
-      const bDate = new Date(b.received);
-
-      return aDate > bDate ? 1 : -1;
-    }));
+    // .sort(((a: any, b: any) => {
+    //   const aDate = new Date(a.received);
+    //   const bDate = new Date(b.received);
+    //
+    //   return aDate > bDate ? 1 : -1;
+    // }));
 
   if (loading) {
     return <Spinner />;
