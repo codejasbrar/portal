628b0e18187f71409d03bf648821bc28661d7129
gauzeee <gauzeee@gmail.com>
2020-07-30 11:08:33 +0300

    fix columns approved mobile

--- src/pages/OrdersPages/TestApprovedPage/TestApprovedPage.tsx
+++ src/pages/OrdersPages/TestApprovedPage/TestApprovedPage.tsx
@@ -192,8 +192,6 @@ const TestApprovedPage = () => {
               </p>
               <p className={styles.mobileOrdersTitle}>Customer
                 ID: <span className={styles.mobileOrdersText}>{item.customerId}</span></p>
-              <p className={styles.mobileOrdersTitle}>Criteria
-                met: <span className={styles.mobileOrdersText}>{item.criteriaMet ? "Yes" : "No"}</span></p>
               <p className={styles.mobileOrdersTitle}>Approved: <span className={styles.mobileOrdersText}>{item.approved.replace('T', ' ')}</span>
               </p>
             </div>
